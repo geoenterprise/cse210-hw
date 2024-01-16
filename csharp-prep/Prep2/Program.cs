058603b555f5160105c99461d3132f7dd47c7167
geoenterprise <jovannyrey@hotmail.com>
2024-01-16 15:15:36 -0500

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What s your grae percentage? ");
+        string grade = Console.ReadLine();
+        int x = int.Parse(grade);
+        if (x >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (x < 90 && x >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+            
+
     }
 }

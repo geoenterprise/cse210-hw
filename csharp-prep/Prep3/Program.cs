d7fc401f4397e14cab7064d8b6fae529fb38fe2c
geoenterprise <jovannyrey@hotmail.com>
2024-01-16 17:21:38 -0500

    Prep 3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int myNumber = randomGenerator.Next(1, 11);
+        Console.Write("What number am I thinking of? ");
+        string answer = Console.ReadLine();
+        int guess = int.Parse(answer);
+        int attempts = 1;
+        if (myNumber == guess)
+        {
+            Console.WriteLine("Awesome guess");
+        }
+        while (myNumber != guess)
+        {
+            Console.Write("What is your new guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (myNumber < guess)
+            {
+                Console.WriteLine("Sorry try a lower number.");
+                attempts += 1;
+            }
+            else if (myNumber > guess)
+            {
+                Console.WriteLine("Sorry try a higher number. ");
+                attempts += 1;
+            }
+            else 
+            {
+                Console.WriteLine("Good job you guessed it.");
+                attempts += 1;
+            }
+                                    
+        }
+        Console.WriteLine($"You tried {attempts} times.");
+                       
     }
 }
